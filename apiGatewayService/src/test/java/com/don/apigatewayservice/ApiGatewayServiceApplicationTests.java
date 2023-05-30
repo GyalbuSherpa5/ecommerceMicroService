72cd47f95e07228054c6636ba9c2249209f7969d
GyalbuSherpa5 <gyalbusherpa555@gmail.com>
2023-05-30 09:54:54 +0545

    api gateway

---
+++ apiGatewayService/src/test/java/com/don/apigatewayservice/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.don.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

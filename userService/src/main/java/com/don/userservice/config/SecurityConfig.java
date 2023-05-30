2706c3be132df1f169de3aa7ed1ddb062d5f8f10
GyalbuSherpa5 <gyalbusherpa555@gmail.com>
2023-05-30 12:28:18 +0545

    add user to api gateway

--- userService/src/main/java/com/don/userservice/config/SecurityConfig.java
+++ userService/src/main/java/com/don/userservice/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;

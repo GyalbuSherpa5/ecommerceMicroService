39628a740bc5809130281c8c23a6ea5dabdf82ef
GyalbuSherpa5 <gyalbusherpa555@gmail.com>
2023-06-12 10:32:12 +0545

    logger added in user service impl

--- userService/src/main/java/com/don/userservice/service/UserCredentialServiceImpl.java
+++ userService/src/main/java/com/don/userservice/service/UserCredentialServiceImpl.java
@@ -5,11 +5,13 @@ import com.don.userservice.exception.UserDoNotExistException;
 import com.don.userservice.model.UserCredential;
 import com.don.userservice.repository.UserCredentialRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserCredentialServiceImpl implements UserCredentialService {
 
     private final UserCredentialRepository userCredentialRepository;
@@ -20,6 +22,7 @@ public class UserCredentialServiceImpl implements UserCredentialService {
     public void saveUser(UserCredential userCredential) {
 
         if (userCredentialRepository.findByUserName(userCredential.getUserName()).isPresent()) {
+            log.error("user already exist in database");
             throw new UserAlreadyExistException("This user already exist");
         }
 
@@ -27,27 +30,36 @@ public class UserCredentialServiceImpl implements UserCredentialService {
                 passwordEncoder.encode(userCredential.getPassword())
         );
         userCredential.setRole("ROLE_USER");
+
         userCredentialRepository.save(userCredential);
+        log.info("user saved successfully");
     }
 
     @Override
     public Long getUserId(String userName) {
+        log.info("Fetching user");
         return userCredentialRepository.getUserId(userName)
-                .orElseThrow(() -> new UserDoNotExistException(
-                        "User with name " + userName + " do not exist")
+                .orElseThrow(() -> {
+                            log.error("user does not exist");
+                            return new UserDoNotExistException(
+                                    "User with name " + userName + " do not exist");
+                        }
                 );
     }
 
     public String generateToken(String userName) {
+        log.info("generating token");
         String role = userCredentialRepository.getUserRole(userName)
-                .orElseThrow(() -> new UserDoNotExistException(
-                        "User with name " + userName + " do not exist")
-                );
+                .orElseThrow(() -> {
+                    log.error("user do not exist");
+                    return new UserDoNotExistException(
+                            "User with name " + userName + " do not exist");
+                });
         return jwtService.generateToken(userName, role);
     }
 
     public void validateToken(String token) {
+        log.info("validating token");
         jwtService.validateToken(token);
     }
-
 }

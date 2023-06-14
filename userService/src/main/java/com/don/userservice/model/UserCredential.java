3f7ebe4035227da2fc123e5ed63e22d50e157dd3
GyalbuSherpa5 <gyalbusherpa555@gmail.com>
2023-06-14 15:35:34 +0545

    not null not blank validation

--- userService/src/main/java/com/don/userservice/model/UserCredential.java
+++ userService/src/main/java/com/don/userservice/model/UserCredential.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,11 +23,13 @@ public class UserCredential {
     private Long userId;
 
     @NotBlank(message = "User name cannot be null or empty")
+    @NotNull(message = "i am don")
     private String userName;
 
     @Email(message = "please enter valid email")
     private String email;
 
+    @Min(value = 5,message = "minimum password length should be 5")
     private String password;
     private String role;
 
